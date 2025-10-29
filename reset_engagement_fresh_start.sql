aa1dc5846c7fba43719f97af21f9b8eab1996ea2
sahilempire <gsahil.sg20@gmail.com>
2025-10-30 02:19:46 +0530

    Improve reset SQL script with column existence check

--- reset_engagement_fresh_start.sql
+++ reset_engagement_fresh_start.sql
@@ -7,9 +7,22 @@ SET likes = 0
 WHERE likes IS NOT NULL;
 
 -- Set views to random value between 30-40 for all articles
+-- Note: If views column doesn't exist, run fix_missing_columns.sql first
 UPDATE twnty_articles 
 SET views = FLOOR(RANDOM() * 11) + 30  -- Random number between 30-40
 WHERE views IS NOT NULL;
+
+-- If views column doesn't exist, this will fail - run fix_missing_columns.sql first
+-- Or use this safer version that handles missing column:
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM information_schema.columns 
+        WHERE table_name = 'twnty_articles' AND column_name = 'views'
+    ) THEN
+        UPDATE twnty_articles SET views = FLOOR(RANDOM() * 11) + 30 WHERE views IS NOT NULL;
+    END IF;
+END $$;
 
 -- Verify the changes
 SELECT 
